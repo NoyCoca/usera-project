c1864e5a306b44966c184b94e95aa0a937640d86
NoyCoca <noycoca123@gmail.com>
2021-04-04 12:22:55 +0300

    del

--- userM.js
+++
@@ -1,9 +0,0 @@
-export default class User{
-    constructor(age, firstName, lastName, email, phone){
-        this.age = age;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-    }
-}
